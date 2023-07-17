d287bd76d2709040ee31b844b68bd40999d0cf6b
Erik Flores Reche <erikfloresreche@gmail.com>
2023-07-17 16:13:34 +0200

    Footer Terminado

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { FaMapMarkerAlt, FaPhone, FaEnvelope } from 'react-icons/fa';
+import { FaMapMarkerAlt, FaPhone, FaEnvelope, FaInstagram, FaTwitter, FaFacebook, FaLinkedin, FaArrowUp } from 'react-icons/fa';
 import '../Footer/Footer.css';
 
 const Footer = () => {
+  const handleBackToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <footer>
+      <div id="backToTop">
+        <a href="#" onClick={handleBackToTop} id="link__backToTop"><FaArrowUp className="backToTop__icon" /> Back to Top</a>
+      </div>
       <div id="container__footer">
-        <div>
+        <div id='container__items__footer'>
           <ul className="contact__list">
-
             <h6 id='contact__footer'>Contact</h6>
             <li className='li__footer__contact'>
               <FaMapMarkerAlt className="contact__icon" />
@@ -24,40 +30,43 @@ const Footer = () => {
             </li>
           </ul>
           
-          <ul className="contact__list">
-            <h6 id='support__footer'>Support</h6>
-            <li className='li__footer__support'>
-              <span>Contact us</span>
-            </li>
-            <li className='li__footer__support'>
-              <span>Web chat</span>
-            </li>
-            <li className='li__footer__support'>
-              <span></span>
-            </li>
-          </ul>
-
           <ul className="followUs__list">
             <h6 id='followUs__footer'>Follow us</h6>
             <li className='li__footer__followUs'>
-              <span>Instagram</span>
+              <FaInstagram className="contact__icon" />
+              <span> Instagram</span>
             </li>
             <li className='li__footer__followUs'>
-              <span>Twitter</span>
+              <FaTwitter className="contact__icon" />
+              <span> Twitter</span>
             </li>
             <li className='li__footer__followUs'>
-              <span>Facebook</span>
+              <FaFacebook className="contact__icon" />
+              <span> Facebook</span>
             </li>
             <li className='li__footer__followUs'>
-              <span>LinkedIn</span>
+              <FaLinkedin className="contact__icon" />
+              <span> LinkedIn</span>
             </li>
           </ul>
 
-          
-
-
-          <p id='copyRight__text'>© 2023 OrangeTracker. All rights reserved.</p>
+          <ul className="contact__list">
+            <h6 id='company__footer'>Company</h6>
+            <li className='li__footer__company'>
+              <span>About us</span>
+            </li>
+            <li className='li__footer__company'>
+              <span>Blog</span>
+            </li>
+            <li className='li__footer__company'>
+              <span>News</span>
+            </li>
+            <li className='li__footer__company'>
+              <span>Support</span>
+            </li>
+          </ul>
         </div>
+        <p id='copyRight__text'>© 2023 OrangeTracker. All rights reserved.</p>
       </div>
     </footer>
   );
